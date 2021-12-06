c98817e1bac814ffe399d0419a4634f0d2dab6f7
SamuelIgwesi <samhillss231@gmail.com>
2021-12-06 12:08:39 +0100

    This is my first commit to github

---
+++ reg.js
@@ -0,0 +1,11 @@
+alert("Fill in the form")
+
+
+var custom = true;
+//block of code to be executed
+if (custom == true) {
+	document.write("You cannot view this site")
+} else if (custom = false) {
+	document.write("You are aloowed")
+
+}
